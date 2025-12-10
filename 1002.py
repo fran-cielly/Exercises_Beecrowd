279dad4a34f82116d190e44d385ed39bfaa063b6
Francielly Santos Carvalho <francielly.carvalho@aluno.ufabc.edu.br>
2025-12-10 18:27:28 -0300

    Problems

---
+++ 1002.py
@@ -0,0 +1,4 @@
+raio = float(input())
+n = 3.14159 #pi
+area = n*pow(raio, 2)
+print(f"A={area:.4f}")
---
+++ 1005.py
@@ -0,0 +1,4 @@
+nota1 = float(input())
+nota2 = float(input())
+media = (3.5*nota1+7.5*nota2)/11
+print(f"MEDIA = {media:.5f}")
